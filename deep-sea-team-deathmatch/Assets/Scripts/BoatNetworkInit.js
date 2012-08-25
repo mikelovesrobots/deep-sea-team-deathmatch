c1fb4c91373746e2a06d42121e873bdedc2a426a
Mike Judge <mikelovesrobots@gmail.com>
2012-08-25 12:28:15 -0700

    Add network rigidbody

--- deep-sea-team-deathmatch/Assets/Scripts/BoatNetworkInit.js
+++ deep-sea-team-deathmatch/Assets/Scripts/BoatNetworkInit.js
@@ -4,12 +4,13 @@ function OnNetworkInstantiate (msg : NetworkMessageInfo) {
   // This is our own player
   if (networkView.isMine) {
     Camera.main.GetComponent(SmoothFollow).target = transform;
-    //gameObject.AddComponent(NetworkSerializationLocal);
+    GetComponent(NetworkRigidbody).enabled = false;
   } else {
     // This is just some remote controlled player, don't execute direct
     // user input on this
     name += "Remote";
     GetComponent(BoatControlScript).enabled = false;
+    GetComponent(NetworkRigidbody).enabled = true;
     //gameObject.AddComponent(NetworkSerializationRemote);
   }
 }
