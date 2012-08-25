23bf8c3f7dae7855d27b8e5d1b5efe21ee091619
Mike Judge <mikelovesrobots@gmail.com>
2012-08-24 22:08:32 -0700

    Better particle effects

--- deep-sea-team-deathmatch/Assets/Scripts/BoatControlScript.js
+++ deep-sea-team-deathmatch/Assets/Scripts/BoatControlScript.js
@@ -1,6 +1,7 @@
 #pragma strict
 
-private var defaultForce : float = 5;
+private var defaultForce : float = 10;
+private var defaultRotation : float = 5;
 
 function Update () {
   var vertical : float = Input.GetAxisRaw("Vertical");
@@ -9,4 +10,5 @@ function Update () {
   var direction : Vector3 = new Vector3(horizontal, 0, vertical);
   Debug.Log(direction);
   rigidbody.AddRelativeForce(direction * defaultForce);
+  rigidbody.AddTorque(new Vector3(0, horizontal, 0) * defaultRotation);
 }
